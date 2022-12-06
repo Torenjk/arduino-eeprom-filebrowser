6299ce6e52abd759115ef35a6ec711df70bbcc35
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-06 17:31:14 +0100

    Create comm_serial.c

---
+++ comm_serial.c
@@ -0,0 +1,14 @@
+/*
+Author: Torenjk (Tom Kamprath)
+
+This file will handle all communication between the client and host
+
+*/
+
+
+void
+parse_files(){
+    //Will send data of the eeprom over to client
+}
+
+//Todo: checksum generation on Client and Host side
