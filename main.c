adf413c3a3dc8c7ea769504b6e2e6a1cde85fc00
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-06 19:34:24 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+Author: Torenjk (Tom Kamprath)
+
+This will be your MAIN arduino file.
+Load this File into your Arduino IDE or compile it using avr-gcc.
+
+This File is supposed to be your Firmware on the ATMEGA328 on the Host side.
+It contains your classic setup and main loop
+*/
+
+
+void setup() {
+    //This will initialize our library
+}
+
+void loop() {  
+    //You can put your additional Code here but depending on the complexity this may reduce performance
+    //Do not use any busy-waiting!
+}
