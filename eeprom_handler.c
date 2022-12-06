f7869bdc4dd525ff5b5b762f8a45bbdc318870d7
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-06 17:30:52 +0100

    Update eeprom_handler.c

--- eeprom_handler.c
+++ eeprom_handler.c
@@ -14,7 +14,7 @@ const uint16_t EEPROM_SIZE = 1024;
 
 
 void 
-eeprom_write(uint8_t *address, uint8_t data){
+eeprom_write_byte(uint8_t *address, uint8_t data){
 //Safety check
 if (address >= EEPROM_SIZE){
     //Call an error handler here!! TODO
@@ -52,3 +52,9 @@ Each 8-Bit Address will equal one "file"
 
 //TODO: Implement
 }
+
+/*
+More TODOs:
+- lock interrupts to make sure the eeprom is not beeing written to
+
+*/
