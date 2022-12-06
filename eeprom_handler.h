54c4d0ede7efd1348473b9d41d9e30abefa6cec1
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-06 17:02:45 +0100

    Update eeprom_handler.h

--- eeprom_handler.h
+++ eeprom_handler.h
@@ -1,1 +1,15 @@
-still empty lol
+/*
+Author: Torenjk (Tom Kamprath)
+Brief Description: Handles all eeprom functions on Host Side
+*/
+
+#pragma once
+
+void eeprom_write_byte(uint8_t *address, uint8_t data)
+    //Will write Data to the eeprom on specified address
+
+void eeprom_read(uint8_t *address)
+    //Will read Data from eeprom on specified address
+
+void map_eeprom(void)
+    //Will map whole eeprom for graphical display on Client
