b73af659777424954115737dd0c8175e5a4a9f58
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-08 21:06:11 +0100

    Update comm_serial.h

--- comm_serial.h
+++ comm_serial.h
@@ -10,3 +10,21 @@ returns true if communication is ok
 returns false if communcation failed
 */
  
+init_usart()
+/*
+Will initialize USART0 for communication
+*/
+
+
+parse_files()
+/*
+Communicates all EEPROM-Cells and their stored value
+*/
+
+
+send_msg(uint8_t msg_len, char msg[])
+/*
+sends a message over USART0
+Parameter msg_len defines the message length in Bits
+Parameter msg contains the actual message as type char
+*/
