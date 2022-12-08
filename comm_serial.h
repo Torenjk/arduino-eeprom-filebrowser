1b6045f84d9e42b2dfc7ad257b159d218d592b9f
Torenjk <33992305+Torenjk@users.noreply.github.com>
2022-12-08 17:19:53 +0100

    Update comm_serial.h

--- comm_serial.h
+++ comm_serial.h
@@ -1,1 +1,12 @@
+/*
+Author: Torenjk (Tom Kamprath)
+Brief Description: Handles all eeprom functions on Host Side
+*/
 
+bool init_eeprom_com(void)
+/*
+Initialize the communication to Client (PC)
+returns true if communication is ok
+returns false if communcation failed
+*/
+ 
